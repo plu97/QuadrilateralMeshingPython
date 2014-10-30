e6332969982148c123ce4403e89513780e9e7b2e
Lu Phillip <plu@easley.(none)>
2014-10-30 00:01:58 -0400

    Actually add gift wrap function

--- debug.py
+++ debug.py
@@ -1,23 +1,36 @@
 from quad_functions import *
 
 def debug():
-        point1 = Point('A', 5, 7)
-        point2 = Point('B', 3, 4)
-        point3 = Point('C', 2, 1)
-        point4 = Point('D', 7, 6)
-        point5 = Point('E', 2, 0)
-        point6 = Point('F', 3, 2)
-        point7 = Point('G', 1, 3)
+    '''
+        point1 = Point(5, 7)
+        point2 = Point(3, 4)
+        point3 = Point(2, 1)
+        point4 = Point(7, 6)
+        point5 = Point(2, 0)
+        point6 = Point(5, 1)
+        point7 = Point(3, 2)
+        point8 = Point(1, 3)
+        point9 = Point(0, 0)
         
-        line1 = Line('AB', point1, point2)
-        line2 = Line('CD', point3, point4)
-        line3 = Line('EF', point5, point6)
+        line1 = Line(point1, point2)
+        line2 = Line(point3, point4)
+        line3 = Line(point7, point8)
+    '''
+    
         
+    point1 = Point(0, 0)
+    point2 = Point(0, 6)
+    point3 = Point(6, 0)
+    point4 = Point(6, 6)
+    point5 = Point(8, 3)
+    
+    point_set = [point1, point2, point3, point4]
+    print gift_wrap(point_set)
+    
+    
         
-        
-        
-        '''
-        point_list = file_to_points(sys.argv[1])
-        line_list = find_lines(point_list)
-        print line_list
-        '''
+    '''
+    point_list = file_to_points(sys.argv[1])
+    line_list = find_lines(point_list)
+    print line_list
+    '''
--- quad_functions.py
+++ quad_functions.py
@@ -1,21 +1,33 @@
 
 class Point(object):
-    def __init__(self, name, input_x, input_y):
-        self.name = name
+    def __init__(self, input_x, input_y):
         self.x = input_x
         self.y = input_y
         
-    @classmethod
-    def from_coordinates(cls, input_x, input_y):
-        cls.x = input_x
-        cls.y = input_y
+    def __str__(self):
+        return "Point object with x = " + str(self.x) + " and y = " + str(self.y)
+        
+    def __repr__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
         
 class Line(object):
-    def __init__(self, name, p1, p2):
-        self.name = name
+    def __init__(self, p1, p2):
         self.point1 = p1
         self.point2 = p2
-        
+    
+    def __str__(self):
+        string = "Line object with Point (" + str(self.point1.x) + "," + str(self.point1.y) + ")"
+        string += "\nand Point (" + str(self.point2.x) + "," + str(self.point2.y) + ")"
+        return string
+
+    def __repr__(self):
+        string = "(" + str(self.point1.x) + "," +str(self.point1.y) + ")"
+        string += " and (" + str(self.point2.x) + "," + str(self.point2.y) + ")"
+        return string
+
 
 def cross_product(line1, line2):
     vector1x = line1.point2.x - line1.point1.x
@@ -25,5 +37,64 @@ def cross_product(line1, line2):
     
     return vector1x*vector2y - vector1y*vector2x
 
-def point_on_left_of_line(line1, point):
-    return cross_product(line1, line(line1.point1, point)) > 0
+def point_on_left_of_line(point, line):
+    return cross_product(line, Line(line.point1, point)) > 0
+
+def gift_wrap(point_list):
+    sorted_list = sorted(point_list, key = lambda point: point.x)
+    polygon = list()
+    initPoint = sorted_list[0]
+    
+    while len(polygon) == 0 or polygon[len(polygon) - 1].point2 != initPoint:
+        if len(polygon) == 0: 
+            print sorted_list[0]
+        else:
+            print polygon[len(polygon) - 1].point2
+        
+        
+        if len(sorted_list) == 1: 
+            finalLine = Line(sorted_list[0], initPoint)
+            polygon.append(finalLine)
+            break
+        
+        if len(polygon) == 0:
+            startingPoint = sorted_list[0]
+        else:    
+            startingPoint = polygon[-1].point2
+
+        
+        sorted_list.remove(startingPoint)
+        tentativeLine = Line(startingPoint, sorted_list[0])
+        num = 0    
+
+        
+        '''    
+        if len(sorted_list) == 2:
+            tentativeLine = Line(startingPoint, sorted_list[1])
+            if point_on_left_of_line(initPoint, tentativeLine):
+                lastLine = Line(startingPoint, initPoint)
+                polygon.append(lastLine)
+                
+            else:
+                penultLine = Line(startingPoint, sorted_list[1])
+                lastLine = Line(sorted_list[1], initPoint)
+                polygon.append(penultLine)
+                polygon.append(lastLine)
+                
+            print "BRGH"
+            break
+        '''
+                
+            
+        for i in range(1, len(sorted_list)):
+            if sorted_list[i] == startingPoint:
+                continue
+            if point_on_left_of_line(sorted_list[i], tentativeLine):
+                tentativeLine = Line(startingPoint, sorted_list[i])
+                num = i
+                
+        polygon.append(tentativeLine)
+            
+    
+    return polygon        
+    
